c1f5274575edc893ac2eaa8163ca3ab0d5087722
Shahrooz Sabet <shahrooz.sabet@gmail.com>
2015-05-11 09:07:06 +0430

    Open Academy at https://www.odoo.com/documentation/8.0/howtos/backend.html (seen 20150511) till Onchange implemented.

---
+++ addons/openacademy/models.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+from openerp import models, fields, api
+
+
+class openacademy(models.Model):
+    _name = 'openacademy.course'
+
+    name = fields.Char(string="Title", required=True)
+    description = fields.Text()
+
+    responsible_id = fields.Many2one('res.users',
+                                     ondelete='set null', string="Responsible", index=True)
+    session_ids = fields.One2many(
+        'openacademy.session', 'course_id', string="Sessions")
+    attendee_ids = fields.Many2many('res.partner', string="Attendees")
+
+
+class Session(models.Model):
+    _name = 'openacademy.session'
+
+    name = fields.Char(required=True)
+    start_date = fields.Date(default=fields.Date.today)
+    duration = fields.Float(digits=(6, 2), help="Duration in days")
+    seats = fields.Integer(string="Number of seats")
+    active = fields.Boolean(default=True)
+
+    instructor_id = fields.Many2one('res.partner', string="Instructor",
+                                    domain=['|', ('instructor', '=', True),
+                                            ('category_id.name', 'ilike', "Teacher")])
+    course_id = fields.Many2one('openacademy.course',
+                                ondelete='cascade', string="Course", required=True)
+    attendee_ids = fields.Many2many('res.partner', string="Attendees")
+
+    taken_seats = fields.Float(string="Taken seats", compute='_taken_seats')
+
+    @api.one
+    @api.depends('seats', 'attendee_ids')
+    def _taken_seats(self):
+        if not self.seats:
+            self.taken_seats = 0.0
+        else:
+            self.taken_seats = 100.0 * len(self.attendee_ids) / self.seats
